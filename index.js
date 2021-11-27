fb8bfa93cb7e1730e5d8296767797a35405dd30c
Maggie McCausland <maggiemccausland@hotmail.com>
2021-11-27 10:31:16 -0700

    added more css styling to demo search card item and to html page, added toggle effect to category drop downs, created footer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Food Card Dropdowns
 document.querySelector(".results-dropdown").addEventListener("click", toggleSearchResults)
 function toggleSearchResults() {
     let content = document.querySelector(".results-list");
@@ -7,7 +8,6 @@ function toggleSearchResults() {
         content.style.display = "none";
     }
 }
-
 
 document.querySelector(".instructions-dropdown").addEventListener("click", toggleInstructions)
 function toggleInstructions() {
@@ -22,6 +22,37 @@ function toggleInstructions() {
 document.querySelector(".ingredients-dropdown").addEventListener("click", toggleIngredients)
 function toggleIngredients() {
     let content = document.querySelector("#ingredients");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+// Category Dropdowns
+document.querySelector(".want-to-try-dropdown").addEventListener("click", toggleWant)
+function toggleWant() {
+    let content = document.querySelector(".want-list");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+document.querySelector(".made-dropdown").addEventListener("click", toggleMade)
+function toggleMade() {
+    let content = document.querySelector(".made-list");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+document.querySelector(".favorites-dropdown").addEventListener("click", toggleFavorites)
+function toggleFavorites() {
+    let content = document.querySelector(".favorites-list");
     if (content.style.display === "none") {
         content.style.display = "block";
     } else {
