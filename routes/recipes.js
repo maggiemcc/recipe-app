fc7b422c3d5193c10410f31fd5d35df5a3c30fb0
Maggie McCausland <maggiemccausland@hotmail.com>
2021-12-02 10:02:37 -0700

    created async await to return all recipes and a router get all recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,3 +5,31 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 const Recipe = require("../models/Recipe");
+
+const getAllRecipes = async () => {
+    const allRecipes = await Recipe.find();
+    return allRecipes;
+  };
+
+// Get all Recipes Recipes
+router.get("/", async (req, res) => {
+    await getAllRecipes()
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((error) => console.log(error));
+});
+
+// Get - Favorites Recipes
+router.get("/", async (req, res) => {
+});
+
+// Get - Want to Try Recipes
+router.get("/", async (req, res) => {
+});
+
+// Get = Have Made Recipes
+router.get("/", async (req, res) => {
+});
+
+module.exports = router;
