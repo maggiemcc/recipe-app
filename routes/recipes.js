5185275be84007a9ef39d488860adb1d04b0f838
Maggie McCausland <maggiemccausland@hotmail.com>
2021-12-02 10:38:06 -0700

    installed mongoose, updated .env and config files, added commented out mongoose code for later use

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-
 const Recipe = require("../models/Recipe");
 
 const getAllRecipes = async () => {
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const recipeRoutes = require("./routes/recipes");
-
-const port = 3000
+const { mongoURI, port } = require("./config");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/recipes', recipeRoutes);
+
+// const mongoose = require("mongoose");
+// const {mongoURI} = require(".config");
+// const uri = `${mongoURI}`;
+// let port = process.env.PORT || 8000;
+let port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.json({message: 'recipe app node js api'});
