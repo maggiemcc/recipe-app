8b263f0a864577f37dd86f3aea993ae6c1fef4e8
Maggie McCausland <maggiemccausland@hotmail.com>
2021-12-02 10:21:58 -0700

    started routes for Put, Delete, and Post

--- routes/recipes.js
+++ routes/recipes.js
@@ -11,7 +11,7 @@ const getAllRecipes = async () => {
     return allRecipes;
   };
 
-// Get all Recipes Recipes
+// Get - all Recipes Recipes
 router.get("/", async (req, res) => {
     await getAllRecipes()
     .then((result) => {
@@ -19,6 +19,16 @@ router.get("/", async (req, res) => {
     })
     .catch((error) => console.log(error));
 });
+
+// Get - Recipe ID
+router.get("/:recipeId", async (req, res) => {
+    await Recipe.findOne({})
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((error) => console.log(error));
+});
+
 
 // Get - Favorites Recipes
 router.get("/favorites/:favorite", async (req, res) => {
@@ -38,9 +48,39 @@ router.get("/try/:try", async (req, res) => {
     .catch((error) => console.log(error));
 });
 
-// Get = Have Made Recipes
+// Get - Have Made Recipes
 router.get("/made/:made", async (req, res) => {
     await Recipe.find({})
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((error) => console.log(error));
+});
+
+// Post - Create/Add Recipe
+router.post("/", async (req, res) => {
+    const newRecipe = await new Recipe({});
+    newRecipe
+    .save()
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((error) => console.log(error));
+});
+
+// Put - Edit Recipe Based On ID
+router.put("/:recipeId", async (req, res) => {
+    await Recipe.findOneAndUpdate({})
+    .then(() => {
+        Recipe.findOne({})
+        .then((result) => res.send(result))
+    })
+    .catch((error) => console.log(error));
+});
+
+// Delete - Delete/Remove Recipe From Category
+router.delete("/:recipeId", async (req, res) => {
+    await Recipe.findOneAndDelete({})
     .then((result) => {
         res.json(result);
     })
