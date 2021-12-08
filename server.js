5a0f229c81a244f82bdeb5a68a156ba3a6f3b6ad
Maggie McCausland <maggiemccausland@hotmail.com>
2021-12-08 14:34:57 -0700

    added express.static public folder, created rough draft recipe model

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const recipeRoutes = require("./routes/recipes");
-const { mongoURI, port } = require("./config");
+const { mongoURI } = require("./config");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/recipes', recipeRoutes);
 
-// const mongoose = require("mongoose");
-// const {mongoURI} = require(".config");
-// const uri = `${mongoURI}`;
-// let port = process.env.PORT || 8000;
-let port = process.env.PORT;
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+const uri = `${mongoURI}`;
+let port = process.env.PORT || 8000;
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`You are connected. Listening on port ${port}.`);
+        });
+
+    })
+    .catch(console.error);
 
 app.get('/', (req, res) => {
-    res.json({message: 'recipe app node js api'});
-})
-
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    res.json({ message: 'recipe app node js api' });
 })
