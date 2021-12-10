9f359c0d242175cc894877e0c8ecd76d14e66f71
Maggie McCausland <maggiemccausland@hotmail.com>
2021-12-10 15:18:30 -0700

    made changes to model, started event listener and function for adding recipes to 'Want To Try'

--- public/index.js
+++ public/index.js
@@ -11,25 +11,25 @@
         }
     }
 
-    document.querySelector(".instructions-dropdown").addEventListener("click", toggleInstructions)
-    function toggleInstructions() {
-        let content = document.querySelector("#instructions");
-        if (content.style.display === "none") {
-            content.style.display = "block";
-        } else {
-            content.style.display = "none";
-        }
-    }
+    // document.querySelector(".instructions-dropdown").addEventListener("click", toggleInstructions)
+    // function toggleInstructions() {
+    //     let content = document.querySelector("#instructions");
+    //     if (content.style.display === "none") {
+    //         content.style.display = "block";
+    //     } else {
+    //         content.style.display = "none";
+    //     }
+    // }
 
-    document.querySelector(".ingredients-dropdown").addEventListener("click", toggleIngredients)
-    function toggleIngredients() {
-        let content = document.querySelector("#ingredients");
-        if (content.style.display === "none") {
-            content.style.display = "block";
-        } else {
-            content.style.display = "none";
-        }
-    }
+    // document.querySelector(".ingredients-dropdown").addEventListener("click", toggleIngredients)
+    // function toggleIngredients() {
+    //     let content = document.querySelector("#ingredients");
+    //     if (content.style.display === "none") {
+    //         content.style.display = "block";
+    //     } else {
+    //         content.style.display = "none";
+    //     }
+    // }
 
     // Category Dropdowns
     document.querySelector(".want-to-try-dropdown").addEventListener("click", toggleWant)
@@ -91,51 +91,52 @@
                 recipeArray = data.meals.map((recipe) => {
                     return {
                         meal: recipe.strMeal,
+                        id: recipe.idMeal,
                         image: recipe.strMealThumb,
                         instructions: recipe.strInstructions,
                         ingredients: {
-                            1: recipe.strIngredient1,
-                            2: recipe.strIngredient2,
-                            3: recipe.strIngredient3,
-                            4: recipe.strIngredient4,
-                            5: recipe.strIngredient5,
-                            6: recipe.strIngredient6,
-                            7: recipe.strIngredient7,
-                            8: recipe.strIngredient8,
-                            9: recipe.strIngredient9,
-                            10: recipe.strIngredient10,
-                            11: recipe.strIngredient11,
-                            12: recipe.strIngredient12,
-                            13: recipe.strIngredient13,
-                            14: recipe.strIngredient14,
-                            15: recipe.strIngredient15,
-                            16: recipe.strIngredient16,
-                            17: recipe.strIngredient17,
-                            18: recipe.strIngredient18,
-                            19: recipe.strIngredient19,
-                            20: recipe.strIngredient20,
+                            one: recipe.strIngredient1,
+                            two: recipe.strIngredient2,
+                            three: recipe.strIngredient3,
+                            four: recipe.strIngredient4,
+                            five: recipe.strIngredient5,
+                            six: recipe.strIngredient6,
+                            seven: recipe.strIngredient7,
+                            eight: recipe.strIngredient8,
+                            nine: recipe.strIngredient9,
+                            ten: recipe.strIngredient10,
+                            eleven: recipe.strIngredient11,
+                            twelve: recipe.strIngredient12,
+                            thirteen: recipe.strIngredient13,
+                            fourteen: recipe.strIngredient14,
+                            fifteen: recipe.strIngredient15,
+                            sixteen: recipe.strIngredient16,
+                            seventeen: recipe.strIngredient17,
+                            eighteen: recipe.strIngredient18,
+                            nineteen: recipe.strIngredient19,
+                            twenty: recipe.strIngredient20,
                         },
                         measurements: {
-                            1: recipe.strMeasure1,
-                            2: recipe.strMeasure2,
-                            3: recipe.strMeasure3,
-                            4: recipe.strMeasure4,
-                            5: recipe.strMeasure5,
-                            6: recipe.strMeasure6,
-                            7: recipe.strMeasure7,
-                            8: recipe.strMeasure8,
-                            9: recipe.strMeasure9,
-                            10: recipe.strMeasure10,
-                            11: recipe.strMeasure11,
-                            12: recipe.strMeasure12,
-                            13: recipe.strMeasure13,
-                            14: recipe.strMeasure14,
-                            15: recipe.strMeasure15,
-                            16: recipe.strMeasure16,
-                            17: recipe.strMeasure17,
-                            18: recipe.strMeasure18,
-                            19: recipe.strMeasure19,
-                            20: recipe.strMeasure20,
+                            one: recipe.strMeasure1,
+                            two: recipe.strMeasure2,
+                            three: recipe.strMeasure3,
+                            four: recipe.strMeasure4,
+                            five: recipe.strMeasure5,
+                            six: recipe.strMeasure6,
+                            seven: recipe.strMeasure7,
+                            eight: recipe.strMeasure8,
+                            nine: recipe.strMeasure9,
+                            ten: recipe.strMeasure10,
+                            eleven: recipe.strMeasure11,
+                            twelve: recipe.strMeasure12,
+                            thirteen: recipe.strMeasure13,
+                            fourteen: recipe.strMeasure14,
+                            fifteen: recipe.strMeasure15,
+                            sixteen: recipe.strMeasure16,
+                            seventeen: recipe.strMeasure17,
+                            eighteen: recipe.strMeasure18,
+                            nineteen: recipe.strMeasure19,
+                            twenty: recipe.strMeasure20,
                         },
                         tutorial: recipe.strYoutube,
                         try: false,
@@ -143,12 +144,96 @@
                         favorite: false,
                     }
                 })
-                console.log("array >", recipeArray);
+                displayRecipes()
+
+                console.log("array ->", recipeArray);
             })
             .catch((error) => {
                 console.error(error);
             });
     }
 
+
+    // displaying content in results
+    function displayRecipes() {
+        recipeArray.forEach((recipe) => {
+            let recipeCard = `
+            <div class="card" id="${recipe.id}">
+            <img src="${recipe.image}" class="card-img-top" alt="temporary">
+            <div class="card-body">
+                <h5 class="card-title">${recipe.meal}</h5>
+                <div>
+                    <div class="instructions-dropdown">
+                        <h6>Cooking Instructions</h6>
+                        <i class="fas fa-caret-down"></i>
+                    </div>
+
+                    <div id="instructions">
+                        <p>${recipe.instructions}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="ingredients-dropdown">
+                        <h6>Ingredients</h6>
+                        <i class="fas fa-caret-down"></i>
+                    </div>
+
+                    <ul class="list-group list-group-flush" id="ingredients">
+                        <li class="list-group-item">${recipe.ingredients.one}</li>
+                        <li class="list-group-item">${recipe.ingredients.two}</li>
+                        <li class="list-group-item">${recipe.ingredients.three}</li>
+                        <li class="list-group-item">${recipe.ingredients.four}</li>
+                        <li class="list-group-item">${recipe.ingredients.five}</li>
+                        <li class="list-group-item">${recipe.ingredients.six}</li>
+                        <li class="list-group-item">${recipe.ingredients.seven}</li>
+                        <li class="list-group-item">${recipe.ingredients.eight}</li>
+                        <li class="list-group-item">${recipe.ingredients.nine}</li>
+                        <li class="list-group-item">${recipe.ingredients.ten}</li>
+                        <li class="list-group-item">${recipe.ingredients.eleven}</li>
+                        <li class="list-group-item">${recipe.ingredients.twelve}</li>
+                        <li class="list-group-item">${recipe.ingredients.thirteen}</li>
+                        <li class="list-group-item">${recipe.ingredients.fourteen}</li>
+                        <li class="list-group-item">${recipe.ingredients.fifteen}</li>
+                        <li class="list-group-item">${recipe.ingredients.sixteen}</li>
+                        <li class="list-group-item">${recipe.ingredients.seventeen}</li>
+                        <li class="list-group-item">${recipe.ingredients.eighteen}</li>
+                        <li class="list-group-item">${recipe.ingredients.nineteen}</li>
+                        <li class="list-group-item">${recipe.ingredients.twenty}</li>
+                    </ul>
+                </div>
+
+                <div class="youtube-video">
+                    <a href="${recipe}" class="card-link" style="text-decoration: none;">Watch Youtube Tutorial</a>
+                </div>
+
+                <div class="card-buttons">
+                    <button class="want-to-try" id="wantBtn">Want to Try</button>
+                    <button id="madeBtn">Have Made</button>
+                </div>
+            </div>
+        </div>
+      `;
+
+            resultsContent.insertAdjacentHTML("beforeend", recipeCard);
+
+        })
+    }
+
+
+    // adding click event for want to try
+    document.addEventListener("click", (e) => {
+        if (e.target.id === "wantBtn") addRecipeToTry(e);
+        else if (e.target.id === "madeBtn") addRecipeToMade(e);
+    })
+
+    // Want To Try list
+    function addRecipeToTry(e) {
+        let recipeID = e.target.parentElement.parentElement.parentElement.id;
+        console.log("added to try:", recipeID)
+        let recipe = recipeArray.find((recipe) => { return recipe.id === Number(recipeID) })
+        if (!recipe.try) {return recipe.try = true;}
+        else { return recipe.try = false };
+    }
 
 })(window);
